3959425d3a1152cbef0c8fbd530c8845cf36498a
utkucelik <utku.celik@dijitalsahne.com>
2024-01-14 21:37:45 +0300

    ..

--- src/Application/EmergencySituation/Queries/GetEmergencySituationsQuery/GetEmergencySituationsResponseDto.cs
+++ src/Application/EmergencySituation/Queries/GetEmergencySituationsQuery/GetEmergencySituationsResponseDto.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using CleanArchitecture.Application.Accounts.Queries.GetAccounts;
+using CleanArchitecture.Domain.Entities;
 using CleanArchitecture.Domain.Enums;
 
 namespace CleanArchitecture.Application.EmergencySituation.Queries.GetEmergencySituationsQuery;
@@ -14,4 +17,11 @@ public class GetEmergencySituationsResponseDto
     public Guid CreatedUser { get; set; }
     public double Longitude { get; set; }
     public double Latitude { get; set; }
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Domain.Entities.EmergencySituation, GetEmergencySituationsResponseDto>();
+        }
+    }
 }
