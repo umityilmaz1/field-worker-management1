8119a42cde27b8046edff8211cc4fc9726d7c39e
Ümit Yılmaz <umit.yilmaz@dijitalsahne.com>
2024-01-14 22:18:00 +0300

    daily working report

---
+++ src/WebApi/Controllers/ReportController.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitecture.Application.Reports.Queries.GetDailyWorkConvexHull;
+using CleanArchitecture.Model.Commons;
+using CleanArchitecture.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+public class ReportController : ApiControllerBase
+{
+    [HttpPost]
+    [Route("[action]")]
+    public async Task<ReturnData<List<GetDailyWorkConvexHullQueryResponseDto>>> GetDailyWorkingLocations(GetDailyWorkConvexHullQuery command)
+    {
+        return await Mediator.Send(command);
+    }
+}
